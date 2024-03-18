f8c3019e987a94e3468a8210707ceeb2128bbc85
LantoZheng <lanto@lanxia.xyz>
2024-03-18 21:39:59 +0800

    Fix bug in login functionality

---
+++ Homeworks/CompuPhysics/fipponachy.m
@@ -0,0 +1,16 @@
+%test code
+%k=[];
+%for i = 1:10
+%    k(i)=fippo(i);
+%end
+%k
+function y = fippo(n)
+    switch n
+        case 1
+            y=1;
+        case 2
+            y=1;
+        otherwise
+            y = fippo(n-1)+fippo(n-2);
+    end
+end
